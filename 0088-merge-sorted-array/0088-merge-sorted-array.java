39ac8cbe514bfd3aedf242a1c6e8d916c65af100
Hyunwook <132888916+mixpuppy@users.noreply.github.com>
2024-01-01 21:41:38 +0900

    Time: 1 ms (35.29%), Space: 42.6 MB (6.99%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,26 @@
+// time complexity : O((m+n)log(m+n))
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int j = 0, i = m; j < n; j++) {
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
+}
+// time complexity : O(m+n)
+class Solution2 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+}
