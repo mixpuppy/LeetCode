2899d26ed067da8b0fa4fecb22c6b5135e4d2343
Hyunwook <132888916+mixpuppy@users.noreply.github.com>
2024-01-17 00:46:09 +0900

    Time: 24 ms (57.20%), Space: 40.1 MB (38.44%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,20 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        int target = start + (end - start)/2;
+        while(start < end) {
+            target = start + (end - start)/2;
+            if(isBadVersion(target)) {
+                end = target;
+            } else {
+                start = target + 1;
+            }
+        }
+        // 리턴값은 target 이면 안된다에 유의
+        return start;
+    }
+}
